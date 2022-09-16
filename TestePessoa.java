612639e78e214d97c8eb7938078f9f40ca7f3996
Samuel Bianch <samuelbianch38@gmail.com>
2022-09-16 08:39:02 -0400

    Organizando IDE

--- TestePessoa.java
+++
@@ -1,9 +0,0 @@
-public class TestePessoa {
-    
-    public static void main(String[] args) {
-        Pessoa p1 = new Pessoa();
-        p1.nome = "Samuel";
-        p1.cpf = 123;
-        p1.ola();
-    }
-}
